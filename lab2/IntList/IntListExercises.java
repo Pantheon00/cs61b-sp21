b8c0c7fec4f9d1ee2340884692a8afb2024aee27
mpollution@163.com <mpollution@163.com>
2025-01-10 22:36:17 +0800

    2025.01.10 Lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,6 +14,7 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        head.first += c;
     }
 
     /**
@@ -26,7 +27,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,7 +54,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -73,9 +76,12 @@ public class IntListExercises {
 
         boolean currElemIsPrime = Primes.isPrime(lst.first);
 
-        if (currElemIsPrime) {
+        while ((currElemIsPrime) && (lst.rest != null)) {
             lst.first *= lst.first;
+            lst = lst.rest;
+            currElemIsPrime = Primes.isPrime(lst.first);
         }
+        lst.first *= lst.first;
 
         return currElemIsPrime || squarePrimes(lst.rest);
     }
